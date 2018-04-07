c94af94e94bdec711c1ffcd90ed6704cd92e588f
Tim Bavaro <tbavaro@gmail.com>
2018-04-07 14:26:04 -0700

    commandline app basically works

--- src/apps/GenerateValidatorsApp.ts
+++ src/apps/GenerateValidatorsApp.ts
@@ -1,9 +1,33 @@
 #!/usr/bin/env node
 
+import * as fs from "fs";
+import * as process from "process";
 import * as yargs from "yargs";
 
-const argv = yargs
-  .option("foo", {})
-  .argv;
+import ValidationGenerator from "../ValidationGenerator";
 
-console.log(argv);
+const myYargs = yargs
+  .usage("Usage: $0 [options] source_file ...")
+  .version(false)
+  .option("output", {
+    string: true,
+    description: "Output filename (as opposed to stdout)"
+  });
+
+const argv = myYargs.argv;
+const sourceFileNames = argv._;
+
+if (sourceFileNames.length < 1) {
+  console.error("No source files specified.");
+  process.exit(1);
+}
+
+const generator = new ValidationGenerator(sourceFileNames);
+const result = generator.serializeValidators() + "\n";
+
+const encoding = "utf-8";
+if (argv.output) {
+  fs.writeFileSync(argv.output, result, encoding);
+} else {
+  process.stdout.write(result, encoding);
+}
